06f7857beea79865733d7c8029337ed17897bd05
Antonio Correia <antonio.alves.correia@gmail.com>
2022-05-24 22:33:58 -0300

    setting up migrations with flask-migrate

--- web/webapp/models.py
+++ web/webapp/models.py
@@ -5,12 +5,15 @@ from flask_security import RoleMixin, UserMixin
 class Institution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True)
+    code = db.Column(db.String(10), unique=True)
     created_date = db.Column(db.DateTime(timezone=True), default=func.now())
     users = db.relationship('User')
+    collections = db.relationship('Collection')
     
 roles_users = db.Table('roles_users',
     db.Column('user_id',db.Integer, db.ForeignKey('user.id')),
     db.Column('role_id',db.Integer, db.ForeignKey('role.id')))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
@@ -27,4 +30,11 @@ class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+
+
+class Collection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    code = db.Column(db.String(10), unique=True)
+    institution_id = db.Column(db.Integer, db.ForeignKey('institution.id'))
 
