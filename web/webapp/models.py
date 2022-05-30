0ce081e52d95e6b350bb1729d51fd96d61d2dded
Antonio Correia <antonio.alves.correia@gmail.com>
2022-05-30 05:08:39 -0300

    adding upload for images and progress bar to overview page

--- web/webapp/models.py
+++ web/webapp/models.py
@@ -37,5 +37,55 @@ class Collection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True)
     code = db.Column(db.String(10), unique=True)
+    catalog_number_regex = db.Column(db.String(150))
+    web_base = db.Column(db.String(150))
+    url_base = db.Column(db.String(150))
     institution_id = db.Column(db.Integer, db.ForeignKey('institution.id'))
+    workflows = db.relationship('Workflow')
 
+class Workflow(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    collection_id = db.Column(db.Integer, db.ForeignKey('collection.id'))
+    tasks = db.relationship('Task')
+    settings = db.relationship('WorkflowSettings')
+
+class WorkflowSettings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workflow_id = db.Column(db.Integer, db.ForeignKey('workflow.id'))
+    category = db.Column(db.String(150))
+    label = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    value = db.Column(db.String(150))
+
+class WorkflowFileType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workflow_id = db.Column(db.Integer, db.ForeignKey('workflow.id'))
+    category = db.Column(db.String(150))
+    label = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    file_regex = db.Column(db.String(150))
+    output_sub_path = db.Column(db.String(150))
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    plugin_name = db.Column(db.String(150))
+    sort_order = db.Column(db.Integer)
+    workflow_id = db.Column(db.Integer, db.ForeignKey('workflow.id'))
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    uploaded_date = db.Column(db.DateTime(timezone=True), default=func.now())
+    med_image_url = db.Column(db.Text) #possible change this to string?
+    thumbnail_url = db.Column(db.Text)
+    barcode = db.Column(db.String(20))
+    collection_id = db.Column(db.Integer, db.ForeignKey('collection.id'))
+    catalog_number = db.Column(db.String(150))
+
+# class RunHistory(db.Model): #this should be general or by file?
+#     id = db.Column(db.Integer, primary_key=True)
+#     start_date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     end_date = db.Column(db.DateTime(timezone=True))
+#     workflow_id = db.Column(db.Integer,db.ForeignKey('workflow.id'))
--- web/webapp/views.py
+++ web/webapp/views.py
@@ -1,30 +1,15 @@
-from flask import Blueprint, redirect, render_template, request, flash, jsonify
+from glob import glob
+import os
+from uuid import uuid4
+from flask import Blueprint, redirect, render_template, request, flash, jsonify, url_for
 from flask_security import login_required, current_user, roles_accepted, SQLAlchemyUserDatastore
-from .models import Collection, Institution, Role, User
+from .models import Collection, Institution, Role, User, Workflow, WorkflowFileType, WorkflowSettings
 from . import db
 from flask_sqlalchemy import orm
 import json
 
 views = Blueprint('views', __name__)
 
-
-# @views.route('/', methods=['GET', 'POST'])
-# @login_required
-# def home():
-#     if request.method == 'POST':
-#         institution = request.form.get('institution')
-
-#         if len(institution) < 1:
-#             flash('Name is too short!', category='error')
-#         else:
-#             new_institution = Institution(name=institution)
-#             db.session.add(new_institution)
-#             db.session.commit()
-#             flash('Institution added!', category='success')
-
-#     institutions = Institution.query.all()
-
-#     return render_template("home.html", user=current_user, institutions=institutions)
 
 @views.route('/profile', methods=['GET','POST'])
 @login_required
@@ -35,7 +20,7 @@ def profile():
         db.session.commit()
         flash('Updated successfully!', category='success')
 
-    return render_template("profile.html",user=current_user)
+    return render_template("/users/profile.html",user=current_user)
 
 @views.route('/users/edit/<userid>', methods=['GET','POST'])
 @roles_accepted('admin')
@@ -108,16 +93,36 @@ def history():
 
     return render_template("history.html",user=current_user)
 
-@views.route('/settings', methods=['GET','POST'])
+@views.route('/workflow-settings/<workflowid>', methods=['GET','POST'])
 # @login_required
-def settings():
+def settings(workflowid = 1):
     if request.method == 'POST':
-        current_user.first_name = request.form.get('firstName')
-        current_user.last_name = request.form.get('lastName')
+        config_format = WorkflowSettings.query.filter_by(name = 'config_format').first()
+        config_format.value = request.form.get('config_format')
+        
+        folder_increment = WorkflowSettings.query.filter_by(name = 'folder_increment').first()
+        folder_increment.value = request.form.get('folder_increment')
+        
+        number_pad = WorkflowSettings.query.filter_by(name = 'number_pad').first()
+        number_pad.value = request.form.get('number_pad')
+        
+        output_base_path = WorkflowSettings.query.filter_by(name = 'output_base_path').first()
+        output_base_path.value = request.form.get('output_base_path')
+        
+        input_path = WorkflowSettings.query.filter_by(name = 'input_path').first()
+        input_path.value = request.form.get('input_path')
+        
         db.session.commit()
+        
         flash('Updated successfully!', category='success')
 
-    return render_template("settings.html",user=current_user)
+    workflow = Workflow.query.filter_by(id = workflowid).first()
+    settings = WorkflowSettings.query.filter_by(workflow_id = workflowid).all()
+    filetypes = WorkflowFileType.query.filter_by(workflow_id = workflowid).all()
+
+    categories = WorkflowSettings.query.group_by(WorkflowSettings.category)
+
+    return render_template("settings.html",user=current_user,settings=settings,filetypes = filetypes, workflow = workflow, categories=categories)
 
 @views.route('/overview', methods=['GET','POST'])
     # @login_required
@@ -235,3 +240,71 @@ def collections(institutionid=None):
     collections = Collection.query.filter_by(institution_id = institutionid).all()
 
     return render_template("collections.html", user=current_user, institution=institution, collections=collections)
+
+
+
+@views.route("/upload", methods=["POST"])
+def upload():
+    """Handle the upload of a file."""
+    form = request.form
+
+    # Create a unique "session ID" for this particular batch of uploads.
+    upload_key = str(uuid4())
+
+    # Is the upload using Ajax, or a direct POST by the form?
+    is_ajax = False
+    if form.get("__ajax", None) == "true":
+        is_ajax = True
+
+    # Target folder for these uploads.
+    target = "webapp/static/uploads/{}".format(upload_key)
+    try:
+        os.mkdir(target)
+    except:
+        if is_ajax:
+            return ajax_response(False, "Couldn't create upload directory: {}".format(target))
+        else:
+            return "Couldn't create upload directory: {}".format(target)
+
+    print("=== Form Data ===")
+    for key, value in list(form.items()):
+        print(key, "=>", value)
+
+    for upload in request.files.getlist("file"):
+        filename = upload.filename.rsplit("/")[0]
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    if is_ajax:
+        return ajax_response(True, upload_key)
+    else:
+        flash('Upload completed!', category='success')
+
+
+@views.route("/files/<uuid>")
+def upload_complete(uuid):
+    
+    # Get their files.
+    root = "webapp/static/uploads/{}".format(uuid)
+    if not os.path.isdir(root):
+        return "Error: UUID not found!"
+
+    files = []
+    for file in glob.glob("{}/*.*".format(root)):
+        fname = file.split(os.sep)[-1]
+        files.append(fname)
+
+    return render_template("files.html",
+        uuid=uuid,
+        files=files,
+    )
+
+
+def ajax_response(status, msg):
+    status_code = "ok" if status else "error"
+    return json.dumps(dict(
+        status=status_code,
+        msg=msg,
+    ))
