6487f90288f0ab5b64002062fa1dceedc80ff389
Joseph Young <joseph@josephayoung.com>
2022-06-01 10:08:38 -0500

    Move out more config to json

--- torch/torch.py
+++ torch/torch.py
@@ -5,30 +5,25 @@ from config.TorchConfig import TorchConfig
 from flows.process_specimen import process_specimen
 # from flows.herbar_flow import herbar_flow
 
-PROJECT_IDS = ['TX', 'ANHC', 'VDB', 'TEST', 'Ferns', 'TORCH', 'EF']
-
 if __name__ == '__main__':
     freeze_support()
 
     description = "Runs the TORCH workflow on a file or directory."
 
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("-d", '--directory', help="Path to the directory that"
-                        "contains the images to be analyzed.")
-    parser.add_argument("-c", '--config', help="the config file to use")
+
+    parser.add_argument("-d", '--directory',
+                        required=True,
+                        help="Path to the directory that contains the images"
+                        "to be analyzed.")
+
+    parser.add_argument("-c", '--config', help="the config file to use",
+                        default='config/config.json')
+
     parser.add_argument("-p", '--parallel',
                         help="process files in parallel",
                         action="store_true")
 
-    parser.add_argument("--project", required=False, choices=PROJECT_IDS,
-                        help="Project name for filtering in database")
-    parser.add_argument("--default_prefix", required=False,
-                        help="Barcode prefix string which will be used as the"
-                        "primary barcode when multiple barcodes are found."
-                        "Suppresses multiple barcode names in filename.")
-    parser.add_argument("-b", "--batch", required=False,
-                        help="Flags written to batch_flags, can be used"
-                        "for filtering downstream data.")
     parser.add_argument("-o", "--output", nargs='?',
                         default='primary',
                         const='secondary',
@@ -38,13 +33,12 @@ if __name__ == '__main__':
                         "If just the -o switch is used, log is written to"
                         "directory indicated in source argument."
                         "An absolute or relative path may also be provided.")
+
     parser.add_argument("-n", "--no_rename", required=False,
                         action='store_true',
                         help="Files will not be renamed, only log"
                         "file generated.")
-    parser.add_argument("--code", required=False,
-                        help="Collection or herbarium code prepended"
-                        "to barcode values.")
+
     parser.add_argument("-v", "--verbose", required=False, action='store_true',
                         help="Detailed output for each file processed.")
 
