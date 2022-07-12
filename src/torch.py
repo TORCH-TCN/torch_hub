491b459bb551509969b147eee044fae3e0662a1e
Joseph Young <joseph@josephayoung.com>
2022-07-12 10:06:09 -0500

    Orchestration of server, agent, and UI

--- src/torch.py
+++ src/torch.py
@@ -1,81 +1,57 @@
+import json
 from multiprocessing.dummy import freeze_support
-import argparse
-from prefect.executors import DaskExecutor, LocalExecutor
-from config.TorchConfig import TorchConfig
-from flows.process_specimen import process_specimen
-from torch.config.database.TorchDatabase import TorchDatabase
+import socket
+import threading
+import time
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_socketio import SocketIO
+from flask_mail import Mail
+from prefect.agent.local import LocalAgent
+from prefect.cli.server import start as start_prefect_server
 
-# from flows.herbar_flow import herbar_flow
+# from torch.config.TorchConfig import TorchConfig
+
+
+# config = TorchConfig.from_json("config.json")
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+login_manager = LoginManager()
+socketio = SocketIO(app)
+
+
+def wait_for_prefect_server():
+    for i in range(120):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            s.connect(("127.0.0.1", int(4200)))
+            s.shutdown(socket.SHUT_RDWR)
+            print("Prefect Server is UP")
+        except OSError:
+            time.sleep(1)
+            print("Prefect Server is not up yet")
+        finally:
+            s.close()
+
 
 if __name__ == "__main__":
     freeze_support()
 
-    description = "Runs the TORCH workflow on a file or directory."
+    app.config.from_file("config.json", load=json.load)
 
-    parser = argparse.ArgumentParser(description=description)
+    server = threading.Thread(target=lambda: start_prefect_server())
+    server.daemon = True
+    server.start()
+    wait_for_prefect_server()
 
-    parser.add_argument(
-        "-d",
-        "--directory",
-        required=True,
-        help="Path to the directory that contains the images" "to be analyzed.",
-    )
+    login_manager.init_app(app)
+    ui = threading.Thread(target=lambda: socketio.run(app))
+    ui.daemon = True
+    ui.start()
 
-    parser.add_argument(
-        "-c", "--config", help="the config file to use", default="config/config.json"
-    )
-
-    parser.add_argument(
-        "-p", "--parallel", help="process files in parallel", action="store_true"
-    )
-
-    parser.add_argument(
-        "-o",
-        "--output",
-        nargs="?",
-        default="primary",
-        const="secondary",
-        help="Path to the directory where log file is written."
-        "By default (no -o switch used) log will be written to"
-        "location of script."
-        "If just the -o switch is used, log is written to"
-        "directory indicated in source argument."
-        "An absolute or relative path may also be provided.",
-    )
-
-    parser.add_argument(
-        "-n",
-        "--no_rename",
-        required=False,
-        action="store_true",
-        help="Files will not be renamed, only log" "file generated.",
-    )
-
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        required=False,
-        action="store_true",
-        help="Detailed output for each file processed.",
-    )
-
-    args = parser.parse_args()
-    config = TorchConfig.from_json(args.config)
-    db = TorchDatabase(config["SQLALCHEMY_DATABASE_URI"])
-
-    # todo adapt to pass parameters through config
-    # herbar_flow.run(path=args.directory,
-    #                 default_prefix=args.default_prefix,
-    #                 no_rename=args.no_rename,
-    #                 code=args.code,
-    #                 verbose=args.verbose,
-    #                 jpeg_rename=args.jpeg_rename,
-    #                 executor=DaskExecutor() if args.parallel
-    #                     else LocalExecutor())
-
-    process_specimen.run(
-        path=args.directory,
-        config=config,
-        db=db,
-        executor=DaskExecutor() if args.parallel else LocalExecutor(),
-    )
+    LocalAgent().start()
