6ba8ae4340bed428f7e9efb4f0dcf7218b364f64
Joseph Young <joseph@josephayoung.com>
2022-11-25 14:03:11 -0600

    Turn TorchHub into a PyPI package

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='torchhub',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['flask'],
+    url='https://torchhub.azurewebsites.net',
+    license='MIT',
+    author='',
+    author_email='',
+    description=''
+)
