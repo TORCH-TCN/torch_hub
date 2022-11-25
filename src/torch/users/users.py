ec5724f258c9d77abd31b65e0452c640db5b4b19
Joseph Young <joseph@josephayoung.com>
2022-11-25 09:44:32 -0600

    Requirements.txt cleanup & Pycharm setup

--- src/torch/users/users.py
+++ src/torch/users/users.py
@@ -1,7 +1,7 @@
 import json
 from flask import Blueprint, flash, jsonify, render_template, request
 from flask_security import current_user, RegisterForm, roles_accepted
-from flask_sqlalchemy import orm
+from sqlalchemy import orm
 from wtforms import StringField
 from torch.users.role import (
     assign_role_to_user,
