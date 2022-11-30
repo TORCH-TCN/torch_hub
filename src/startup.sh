a81627e399823aebe1308c2d4cbb062b2557f5bf
Joseph Young <joseph@josephayoung.com>
2022-11-30 10:56:08 -0600

    Change to GeventWebSocketWorker for websocket support

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get -y update
 apt-get -y install zbar-tools
-gunicorn --bind=0.0.0.0 --timeout 600 -k gevent -w 1 app:app
+gunicorn --bind=0.0.0.0 --timeout 600 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 app:app
