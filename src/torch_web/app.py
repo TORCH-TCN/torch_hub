dc162023cfb1b031318bd59be0763e9d0fb833f4
Joseph Young <joseph@josephayoung.com>
2023-02-22 09:34:08 -0600

    Fix circular reference

--- src/torch_web/app.py
+++ src/torch_web/app.py
@@ -3,16 +3,17 @@ from flask_mail import Mail
 from flask_socketio import SocketIO
 from torch_web import create_app
 
-app = create_app()
-socketio = SocketIO(app)
-mail = Mail(app)
-
-
 if __name__ == "__main__":
     freeze_support()
+    app = create_app()
+    socketio = SocketIO(app)
+    mail = Mail(app)
 
     app.app_context().push()
     socketio.run(app)
 else:
+    app = create_app()
+    mail = Mail(app)
+
     freeze_support()
     app.app_context().push()
